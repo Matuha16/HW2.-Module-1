1291c431ea1cad6b019ce245167ccbcb52af1a54
Matuha16 <matveevkarton03@gmail.com>
2024-10-07 15:16:19 +0300

    Add files via upload

---
+++ ДЗПеременные.py
@@ -0,0 +1,7 @@
+number_of_completed_HW = 12
+print('Количество выполненных ДЗ:', number_of_completed_HW)
+hours_spent = 1.5
+print('Количество затраченных часов:', hours_spent)
+course_name = 'Python'
+print('Название курса:', course_name)
+print('Курс:', course_name, ', всего задач:', number_of_completed_HW, ', затрачено часов:', hours_spent, ', среднее время выполнения:', hours_spent / number_of_completed_HW, 'часа.')
